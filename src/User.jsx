cca713b75c7fd02010e13558a74bbdb3a2d98d7c
Me <sorry@404.com>
2025-03-16 19:08:08 +0600

    Table Style

--- src/User.jsx
+++ src/User.jsx
@@ -1,7 +1,45 @@
 export default function User(){
     return (
         <>
-            <h1>This is user page</h1>
+            <br />
+            <h2>User Management</h2>
+            
+            <div className="MTB flex FY-center F-space">
+            <input type="search" placeholder="Search by user name" />
+            <button>Add User</button>
+            </div>
+
+            <table border="1">
+                <thead>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Parvez Ahmed</td>
+                        <td>parvez@gmail.com</td>
+                        <td>Frontend Dev</td>
+                        <td>
+                            <a href="#edit">Edit</a>
+                            &nbsp;
+                            <a href="#delete">Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Parvez Ahmed</td>
+                        <td>parvez@gmail.com</td>
+                        <td>Frontend Dev</td>
+                        <td>
+                            <a href="#edit">Edit</a>
+                            &nbsp;
+                            <a href="#delete">Delete</a>
+                        </td>
+                    </tr>
+                    
+                </tbody>
+            </table>
         </>
     );
 }
